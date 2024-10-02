abb7a773058a055dad71290eee9eabe424c3c546
Cassius <Theirongiant777@gmail.com>
2024-10-02 13:02:56 -0700

    finished all tasks

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,18 @@ function moduleProject3() {
 
   function buildNav(links) {
     //  ✨ do your magic here
-    return document.createElement('nav')
+
+    const nav = document.createElement('nav');
+
+    links.forEach(link => {
+    const anchor = document.createElement('a');
+      anchor.href = link.href;
+      anchor.setAttribute('title', link.title);
+      anchor.textContent = link.textContent;
+      nav.appendChild(anchor);
+    })
+
+    return nav
   }
 
   // ❗ DOM creation using your `buildNav` component (do not change):
@@ -20,6 +31,39 @@ function moduleProject3() {
 
   function buildLearnerCard(learner, languages) {
     //  ✨ do your magic here
+
+    const div = document.createElement('div');
+    div.classList.add('learner-card');
+
+    const name = document.createElement('p');
+    name.textContent = learner.fullName;
+
+    const id = document.createElement('p');
+    id.textContent = `Learner ID: ${learner.id}`;
+
+    const birth = document.createElement('p');
+    birth.textContent = `Date of Birth: ${learner.dateOfBirth}`;
+
+    const language = languages.find(lang => lang.id === learner.favLanguage);
+    const fav = document.createElement('p');
+    fav.textContent = `Favorite Language: ${language ? language.name : 'No'}`;
+
+    div.appendChild(name);
+    div.appendChild(id);
+    div.appendChild(birth);
+    div.appendChild(fav);
+    document.querySelector('section').appendChild(div);
+
+
+    div.addEventListener('click', function() {
+      const activeCard = document.querySelector('.active');
+      if (activeCard) {
+        activeCard.classList.remove('active');
+      } 
+      div.classList.add('active');
+    });
+
+    return div;
   }
 
   {
@@ -40,13 +84,68 @@ function moduleProject3() {
       { id: 17, fullName: 'Daniel Castillo', dateOfBirth: '1995-11-05', favLanguage: 12 }
     ]
     //  ✨ do your magic here
+
+    learners.forEach(ele => {
+      buildLearnerCard(ele, languages);
+    })
   }
 
   // 👉 TASK 3 - Write a `buildFooter` component that returns a footer
 
   function buildFooter(footerData) {
     //  ✨ do your magic here
-    return document.createElement('footer')
+
+    const footer = document.createElement('footer');
+
+    const footerDiv = document.createElement('div');
+    footerDiv.classList.add('company-info');
+
+    const compName = document.createElement('p');
+    compName.classList.add('company-name');
+    compName.textContent = footerData.companyName;
+
+    const add = document.createElement('p');
+    add.classList.add('address');
+    add.textContent = footerData.address;
+
+    const email = document.createElement('p');
+    email.classList.add('contact-email');
+    email.textContent = 'Email: ';
+
+    const emailLink = document.createElement('a');
+    emailLink.textContent = footerData.contactEmail;
+    emailLink.href = `mailto:${footerData.contactEmail}`;
+
+    const socialDiv = document.createElement('div');
+    socialDiv.classList.add('social-media');
+
+    const twitt = document.createElement('a');
+    twitt.href = footerData.socialMedia.twitter;
+    twitt.textContent = 'Twitter';
+
+    const face = document.createElement('a');
+    face.href = footerData.socialMedia.facebook;
+    face.textContent = 'Facebook';
+
+    const insta = document.createElement('a');
+    insta.href = footerData.socialMedia.instagram;
+    insta.textContent = 'Instagram';
+
+    const bloomDiv = document.createElement('div');
+    bloomDiv.textContent = '© BLOOM INSTITUTE OF TECHNOLOGY 2024';
+
+    footer.appendChild(footerDiv);
+    footer.appendChild(socialDiv);
+    footer.appendChild(bloomDiv);
+    footerDiv.appendChild(compName);
+    footerDiv.appendChild(add);
+    footerDiv.appendChild(email);
+    email.appendChild(emailLink);
+    socialDiv.appendChild(twitt);
+    socialDiv.appendChild(face);
+    socialDiv.appendChild(insta);
+
+    return footer;
   }
 
   // ❗ DOM creation using your `buildFooter` component (do not change):
@@ -64,6 +163,18 @@ function moduleProject3() {
   // 👉 TASK 4 - Clicking on the section should deactivate the active card
 
   //  ✨ do your magic here
+
+  document.querySelector('section').addEventListener('click', function(event) {
+    const opened = document.querySelector('.active');
+    if (opened) {
+      opened.classList.remove('active');
+    }
+
+    if (event.target.closest('.learner-card')) {
+      event.target.closest('.learner-card').classList.add('active');
+    }
+  })
+
 }
 
 // ❗ DO NOT CHANGE THIS CODE
